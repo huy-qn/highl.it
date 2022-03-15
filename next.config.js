f393cdc63cd50c237172dc928082212eea116861
Huy <kingbazoka@gmail.com>
2022-03-15 16:31:49 -0700

    Add rewrite rule to support desired URL scheme

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  rewrites: async () => [
+    {
+      source: '/:url(https.*)',
+      destination: '/view?url=:url'
+    }
+  ]
 }
 
 module.exports = nextConfig
